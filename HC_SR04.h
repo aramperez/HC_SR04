3b81d987439f14d4da15821690c5a322fd385990
Aram Perez <aramperez@mac.com>
2016-11-25 15:56:35 -0500

    Initial checkin.

---
+++ HC_SR04.h
@@ -0,0 +1,73 @@
+#ifndef HC_SR04_H
+#define HC_SR04_H
+/*
+  A simple class for an HC_SR04 ultrasonic sensor, primarily for use with Arduinos.
+  Written by Aram Perez, aramperez at mac dot com.
+
+  The MIT License (MIT)
+  Copyright © 2016 Aram Perez
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation   files (the “Software”), to
+  deal in the Software without restriction, including without limitation the
+  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+  sell copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE.    
+*/
+
+#include <Arduino.h>
+
+class HC_SR04 {
+    uint8_t _trigPin;
+    uint8_t _echoPin;
+    unsigned long _delay;
+
+    HC_SR04();
+    unsigned long microSeconds()
+    {
+      digitalWrite(_trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(_trigPin, LOW);
+
+      unsigned long ms = pulseIn(_echoPin, HIGH, _delay);
+      if( ms == 0 ){
+      	return (unsigned long) -1;
+      }
+      return ms;
+    }
+
+  public:
+    HC_SR04(uint8_t trigPin, uint8_t echoPin, unsigned long delay = 1000) :
+      _trigPin(trigPin),
+      _echoPin(echoPin),
+      _delay(delay)
+    {
+      pinMode(trigPin, OUTPUT);
+      digitalWrite(trigPin, LOW);
+      pinMode(echoPin, INPUT);
+    }
+    virtual ~HC_SR04() {};
+    inline void setDelay(unsigned long delay){_delay = delay;}
+    inline unsigned long inches()
+    {
+      return (unsigned long) (microSeconds() / 148);  //Per data sheet
+    }
+    inline unsigned long centimeters()
+    {
+      return (unsigned long) (microSeconds() / 58);  //Per data sheet
+    }
+};
+
+#endif //HC_SR04_H
+
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# HC_SR04
-A simple class for an HC SR04 ultrasonic sensor, primarily for Arduinos.
+# ElapsedTimer
+A simple class for an HC SR04 ultrasonic sensor, primarily for use with Arduinos.
+
+# Description
+## HC\_SR04(_trigPin_, _echoPin_, _delay_)
+Creates an HC_SR04 object (constructor).
+### Arguments
+* uint8_t _trigPin_: number of the pin connected to the trigger.
+* uint8_t long _echoPin_: number of the pin where the echo delay comes back on.
+* unsigned long _delay_: the delay, in milliseconds, to wait for the echo. Default is 1000 ms.
+
+## void setDelay(_delay_)
+Sets a new delay for listening for the echo.
+### Arguments
+* unsigned long _delay_: delay in milliseconds.
+
+## unsigned long inches()
+Returns distanced measured by the ultrasonic sensor in inches. If no echo was received in the _delay_ time, then (unsigned long) -1 is returned.
+
+## unsigned long centimeters()
+Returns distanced measured by the ultrasonic sensor in centimeters. If no echo was received in the _delay_ time, then (unsigned long) -1 is returned.
